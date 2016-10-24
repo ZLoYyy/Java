6d634daeb8f99a90242268500920b5b2541f6c7f
ZLoYyy <qzloyp@gmail.com>
2016-10-24 17:45:55 +0900

    Create class onRepain

--- GameWindow/src/Games/GameWindow/GameWindow.java
+++ GameWindow/src/Games/GameWindow/GameWindow.java
@@ -1,6 +1,7 @@
 package Games.GameWindow;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow extends JFrame
 {
@@ -9,11 +10,29 @@ public class GameWindow extends JFrame
 
     public static void main(String[] args)
     {
-        game_window = new GameWindow();
-        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        game_window.setLocation(200, 100);
-        game_window.setSize(600, 400);
-        game_window.setVisible(true);
-        game_window.setResizable(false);
+        game_window = new GameWindow();//класс + сылка объект окна
+        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//настройка окна при закрытие
+        game_window.setLocation(200, 100)//где будет появлятся окно
+        game_window.setSize(600, 400)// размер окна;
+        GameField game_field = new GameField();
+        game_window.add(game_field);
+        game_window.setVisible(true);// сделать видимым окно
+        game_window.setResizable(false);// запретить изменение окна
+    }
+
+    private static void onRepaint(Graphics g)// рисовать
+    {
+        g.fillOval(10, 10, 200, 100);
+        g.drawLine(200, 300, 200, 100);
+    }
+
+    private static class GameField extends JPanel //класс понели
+    {
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);//сначало он
+            onRepaint(g);
+        }
     }
 }
