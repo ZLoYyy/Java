3fa2af4547aec47a10fb867b324892d443e5fd26
ZLoYyy <qzloyp@gmail.com>
2017-02-10 19:06:36 +0900

    Переверни массив

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task07.task0704;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Переверни массив
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[array.length - 1 - i]);
+        }
+    }
+}
+
