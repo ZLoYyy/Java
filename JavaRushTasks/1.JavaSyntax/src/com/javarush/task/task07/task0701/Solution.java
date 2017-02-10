63291e37d663c48442c3389a9d054d1a70e0518d
ZLoYyy <qzloyp@gmail.com>
2017-02-10 19:06:20 +0900

    Массивный максимум

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task07.task0701;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Массивный максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] array = initializeArray();
+        int max = max(array);
+        System.out.println(max);
+    }
+
+    public static int[] initializeArray() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] people = new int[20];
+        for (int i = 0; i < people.length; i++) {
+            people[i] = Integer.parseInt(reader.readLine());
+        }
+        return people;
+    }
+
+    public static int max(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
