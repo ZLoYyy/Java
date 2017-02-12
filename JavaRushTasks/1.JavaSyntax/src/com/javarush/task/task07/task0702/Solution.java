41cdc009fd265fb3553176aae755f70a8ab3ff9f
ZLoYyy <qzloyp@gmail.com>
2017-02-12 15:52:34 +0900

    Массив из строчек в обратном порядке

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0702/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task07.task0702;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/* 
+Массив из строчек в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] list = new String[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 8; i++) {
+            list[i] = reader.readLine();
+        }
+
+        int index = list.length;
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[index - 1 -i]);
+        }
+
+    }
+}
