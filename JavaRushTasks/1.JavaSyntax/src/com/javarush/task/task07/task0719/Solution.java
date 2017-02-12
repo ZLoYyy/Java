57ef856aeab33b8bd2f66e6c318b2f556d1b5e4e
ZLoYyy <qzloyp@gmail.com>
2017-02-12 16:01:19 +0900

    Вывести числа в обратном порядке

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вывести числа в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+
+        int index = list.size();
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(index - 1 -j));
+        }
+    }
+}
