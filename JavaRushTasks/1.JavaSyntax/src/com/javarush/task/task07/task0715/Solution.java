4a2dffd27d7abbedfa82ecc4f4014e5b4dd8bfdb
ZLoYyy <qzloyp@gmail.com>
2017-02-26 11:40:43 +0900

    Продолжаем мыть раму

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task07.task0715;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Продолжаем мыть раму
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("мама");
+        list.add("мыла");
+        list.add("раму");
+
+        for (int i = 0; i < list.size(); i++) {
+            list.add(i+1, "именно");
+            i++;
+        }
+
+        for (String printList: list) {
+            System.out.println(printList);
+        }
+    }
+}
