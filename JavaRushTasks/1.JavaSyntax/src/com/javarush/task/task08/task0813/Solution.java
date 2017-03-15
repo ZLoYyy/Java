69cde0e61cc1271a086d39d13831546a478334d8
ZLoYyy <qzloyp@gmail.com>
2017-03-15 22:32:24 +0900

    20 слов на букву «Л»

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static HashSet<String> createSet() {
+        HashSet<String> set = new HashSet<>();
+        set.add("Лес");
+        set.add("Лед");
+        set.add("Лама");
+        set.add("Люди");
+        set.add("Лож");
+        set.add("Лиза");
+        set.add("Лом");
+        set.add("Лада");
+        set.add("Левитация");
+        set.add("Лесть");
+        set.add("Ломоть");
+        set.add("Луу");
+        set.add("Лфцвфц");
+        set.add("Лф");
+        set.add("Лфв");
+        set.add("Лфцв");
+        set.add("Лымф");
+        set.add("Лапров");
+        set.add("Лфкыакпв");
+        set.add("Лсуаф");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
