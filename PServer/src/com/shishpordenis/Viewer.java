97b228d1059453e18c823918fb63cbf2c1030279
ZLoYyy <qzloyp@gmail.com>
2017-06-05 20:22:54 +0900

    Viewer

--- PServer/src/com/shishpordenis/Viewer.java
+++ PServer/src/com/shishpordenis/Viewer.java
@@ -1,51 +1,43 @@
-package com.shishpordenis;
-
-import com.sun.net.httpserver.HttpExchange;
-import freemarker.template.TemplateException;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-/**
- * Created by ((ZLoY)) on 23.05.2017.
- */
-public class Viewer extends TemplatedController {
-
-    public Viewer() throws IOException {
-        super();
-    }
-
-    @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
-
-        Printers printers = new Printers();
-        String view = printers.get();
-
-        byte[] bytes = view.getBytes();
-        httpExchange.sendResponseHeaders(200, bytes.length);
-
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(bytes);
-        os.close();
-
-        try {
-            this.respond(view);
-        } catch (TemplateException e) {
-            e.printStackTrace();
-        }
-
-//       new Converter("R:/Projects/JavaConventer/hello.html");
+//package com.shishpordenis;
 //
-//       try {
-//            Printers.print();
-//        } catch (Exception e) {
+//import com.sun.net.httpserver.HttpExchange;
+//import freemarker.template.TemplateException;
+//
+//import javax.xml.transform.TransformerConfigurationException;
+//import java.io.IOException;
+//import java.io.OutputStream;
+//
+///**
+// * Created by ((ZLoY)) on 23.05.2017.
+// */
+//public class Viewer extends TemplatedController {
+//
+//    public Viewer() throws IOException {
+//        super();
+//    }
+//
+//    @Override
+//    public void handle(HttpExchange httpExchange) throws IOException {
+//
+//        Printers printers = new Printers();
+//        String view = printers.get();
+//
+//        byte[] bytes = view.getBytes();
+//        httpExchange.sendResponseHeaders(200, bytes.length);
+//
+//        OutputStream os = httpExchange.getResponseBody();
+//        os.write(bytes);
+//        os.close();
+//
+//        try {
+//            this.respond(view);
+//        } catch (TemplateException e) {
 //            e.printStackTrace();
 //        }
-
-    }
-
-    @Override
-    protected String getTemplateName() {
-        return "view.ftl";
-    }
-}
+//
+//
+//    @Override
+//    protected String getTemplateName() {
+//        return "view.ftl";
+//    }
+//}
