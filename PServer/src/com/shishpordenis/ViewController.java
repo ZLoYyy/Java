cfa42643a6e9f1b773f8c3d472890ba04f5099a2
ZLoYyy <qzloyp@gmail.com>
2017-06-05 20:20:43 +0900

    ViewController

---
+++ PServer/src/com/shishpordenis/ViewController.java
@@ -0,0 +1,34 @@
+package com.shishpordenis;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+/**
+ * Created by ((ZLoY)) on 02.06.2017.
+ */
+public class ViewController extends TemplatedController {
+    public ViewController() throws IOException {
+    }
+
+    @Override
+    protected String getTemplateName() {
+        return "view.ftl";
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        Executor executor = new Executor();
+        System.out.println("124");
+
+
+        try {
+
+            new Converter("R:/Projects/JavaConventer/Free Online .html");
+            Printers.print(executor.getPDFName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
