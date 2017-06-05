a9a9e0cfd38834488fe8166994f80d247ed9ee59
ZLoYyy <qzloyp@gmail.com>
2017-06-05 20:19:25 +0900

    Executor

---
+++ PServer/src/com/shishpordenis/Executor.java
@@ -0,0 +1,16 @@
+package com.shishpordenis;
+
+/**
+ * Created by ((ZLoY)) on 02.06.2017.
+ */
+public class Executor {
+    private static String pdf = null;
+
+    public void setPDFName(String pdfName) {
+        pdf = pdfName;
+    }
+    //
+    public String getPDFName(){
+        return pdf;
+    }
+}
